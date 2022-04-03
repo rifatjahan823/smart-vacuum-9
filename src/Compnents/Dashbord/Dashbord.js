98d95b22c6b4a49b5b74481c7d59065659f3c1ce
rifatjahan823 <rifatjahan823@gmail.com>
2022-04-04 03:47:18 +0600

    complete dashbord

--- src/Compnents/Dashbord/Dashbord.js
+++ src/Compnents/Dashbord/Dashbord.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Area, AreaChart, CartesianGrid, Tooltip, XAxis, YAxis } from 'recharts';
+import { Area, AreaChart, Bar, BarChart, CartesianGrid, ComposedChart, Label, LabelList, Legend, Line, LineChart, Pie, PieChart, Tooltip, XAxis, YAxis } from 'recharts';
 
 const Dashbord = () => {
     const data = [
@@ -41,26 +41,57 @@ const Dashbord = () => {
         },
       ]
     return (
-        <div>
-  <AreaChart width={730} height={250} data={data}
-  margin={{ top: 10, right: 30, left: 0, bottom: 0 }}>
-  <defs>
-    <linearGradient id="colorinvestment" x1="0" y1="0" x2="0" y2="1">
-      <stop offset="5%" stopColor="#8884d8" stopOpacity={0.8}/>
-      <stop offset="95%" stopColor="#8884d8" stopOpacity={0}/>
-    </linearGradient>
-    <linearGradient id="colorsell" x1="0" y1="0" x2="0" y2="1">
-      <stop offset="5%" stopColor="#82ca9d" stopOpacity={0.8}/>
-      <stop offset="95%" stopColor="#82ca9d" stopOpacity={0}/>
-    </linearGradient>
-  </defs>
-  <XAxis dataKey="month" />
-  <YAxis />
-  <CartesianGrid strokeDasharray="3 3" />
-  <Tooltip />
-  <Area type="monotone" dataKey="investment" stroke="#8884d8" fillOpacity={1} fill="url(#colorinvestment)" />
-  <Area type="monotone" dataKey="sell" stroke="#82ca9d" fillOpacity={1} fill="url(#colorsell)" />
-</AreaChart> 
+        <div className='container'>
+        <div className='row mt-5'>
+             <div className='col-lg-6 col-md-6 col-sm-12'>
+             <ComposedChart width={500} height={250} data={data}>
+            <XAxis dataKey="month" />
+            <YAxis />
+            <Tooltip />
+            <Legend />
+            <CartesianGrid stroke="#f5f5f5" />
+            <Area type="monotone" dataKey="revenue" fill="#8884d8" stroke="#8884d8" />
+            <Bar dataKey="investment" barSize={20} fill="#413ea0" />
+            <Line type="monotone" dataKey="sell" stroke="#ff7300" />
+            </ComposedChart>
+            </div> 
+            <div className='col-lg-6 col-md-6 col-sm-12'>
+            <PieChart width={500} height={250}>
+            <Pie data={data} dataKey="revenue" nameKey="month" cx="50%" cy="50%" outerRadius={50} fill="#8884d8" />
+            <Pie data={data} dataKey="revenue" nameKey="month" cx="50%" cy="50%" innerRadius={60} outerRadius={80} fill="#82ca9d" label />
+            </PieChart>
+            </div>
+            <div className='col-lg-6 col-md-6 col-sm-12 mt-5'>
+            <LineChart width={500} height={250} data={data}
+            margin={{ top: 5, right: 30, left: 20, bottom: 5 }}>
+            <CartesianGrid strokeDasharray="3 3" />
+            <XAxis dataKey="month" />
+            <YAxis />
+            <Tooltip />
+            <Legend />
+            <Line type="monotone" dataKey="sell" stroke="#8884d8" />
+            <Line type="monotone" dataKey="investment" stroke="#82ca9d" />
+            </LineChart>
+            </div>
+            <div className='col-lg-6 col-md-6 col-sm-12 mt-5'>
+            <BarChart
+            width={500}
+            height={250}
+            data={data}
+            margin={{ top: 15, right: 30, left: 20, bottom: 5 }}
+            >
+            <CartesianGrid strokeDasharray="3 3" />
+            <XAxis dataKey="month">
+            </XAxis>
+            <Bar dataKey="sell" fill="#8884d8">
+                <LabelList dataKey="name" position="insideTop" angle="45"  />
+            </Bar>
+            <Bar dataKey="investment" fill="#82ca9d">
+                <LabelList dataKey="investment" position="top" />
+            </Bar>
+            </BarChart>
+            </div>
+        </div>
         </div>
     );
 };
